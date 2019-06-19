da53f8ed405ac8d46b3d23beb84afe9103eece83
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-19 17:14:48 +0200

    Add files via upload

---
+++ Chapter4Task1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter4Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+      
+            Console.WriteLine("Select number");
+            String myStr = Console.ReadLine();;
+            bool result = double.TryParse(myStr, out num);
+            if (result == true)       
+                Console.WriteLine(Math.Pow(num,5));
+            else
+                Console.WriteLine("false");
+            Console.ReadLine();
+                   
+            
+        }
+    }
+}
