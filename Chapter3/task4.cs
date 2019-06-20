6d79c9832edc0fea1e615bfc023c35010fc04cf7
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-20 22:41:12 +0200

    initual repositoriy setup

---
+++ Chapter3/task4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SchoolBox.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string myString = "Superman";
+            string upper = myString.ToUpper();
+            Console.WriteLine(upper);
+
+          
+
+        }   
+
+
+
+
+
+
+
+    }
+}
