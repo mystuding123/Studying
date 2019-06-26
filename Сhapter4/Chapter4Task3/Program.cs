aa6c8841dae1d9c3cdc3e519ed64830a45c22f6e
lolissimo2402 <51722600+lolissimo2402@users.noreply.github.com>
2019-06-26 20:50:55 +0200

    no message

---
+++ Сhapter4/Chapter4Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Сhapter4Task3
+{
+    class Program
+    {
+        static int AddInt(params int[] numbers)
+        {
+            Console.WriteLine("Write {0} ints.", numbers.Length);
+
+            int sum = 0;
+            for (int a = 0; a < numbers.Length; a++)
+                sum += numbers[a];
+
+            return sum;
+
+        }
+        static void Main(string[] args)
+        {
+            int sum;
+            sum = AddInt(3, 5, 7, 10);
+            Console.WriteLine("Summa ints: {0} ", sum);
+
+            Console.ReadLine();
+        }
+    }
+}
