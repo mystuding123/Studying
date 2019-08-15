115c47022d1fb15015ed377fda7a522434d07538
lolissimo2 <lolissimo2402@gmail.com>
2019-08-15 21:08:15 +0300

    Task8

---
+++ Сhapter4/Chapter4.Task8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//
+namespace Chapter4.Task8
+{
+    class Program
+    {
+        static void EvaluateEnum(System.Enum e)
+        {
+
+            Array enumData = Enum.GetValues(e.GetType());
+
+            Console.WriteLine("as num {0}", enumData.Length);
+
+            for (int i = 0; i < enumData.Length; i++)
+            {
+                Console.WriteLine("name: {0}, value: {0:D}", enumData.GetValue(i));
+            }
+
+            Console.WriteLine();
+
+        }
+        static void Main(string[] args)
+        {
+            ConsoleColor cc = ConsoleColor.Red;
+
+            EvaluateEnum(cc);
+
+            Console.ReadLine();
+        }
+    }
+}
