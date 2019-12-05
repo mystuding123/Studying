f62d5a65d219578152aad9e3b67f96312087694f
lolissimo2 <lolissimo2402@gmail.com>
2019-12-05 21:24:35 +0200

    Chapter3.Task4 is done

---
+++ Chapter3/Chapter3.Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SchoolBox.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string myString = "Superman";
+
+            string upper = myString.ToUpper();
+
+            Console.WriteLine(upper);
+        }
+    }
+}
