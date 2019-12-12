3e3b3db5339df895d8255bfc47a1d0ac41de8004
Lolissimo2402 <lolissimo2402@gmail.com>
2019-12-12 14:28:00 +0200

    task2 and gitignore

--- Chapter3/Chapter3.Task2/Program.cs
+++ Chapter3/Chapter3.Task2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+//2. Зчитати стрічку з екрану, створити метод який вертає стрічку без пробілів
+namespace Chapter3.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the text");
+            string firstStr = Console.ReadLine();
+
+            firstStr = firstStr.Replace(" ", "");
+            Console.WriteLine(firstStr);
+
+            Console.ReadLine();
+        }
+    }
+}
