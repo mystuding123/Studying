8ecf5f81ae2132a25afdebd8e707c862f6b5ae02
lolissimo2402 <lolissimo2402@gmail.com>
2020-01-16 22:13:56 +0200

    Chapter4.Task10 Start

---
+++ Сhapter4/Chapter4.Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+//10. Написати метод, який приймає приймає налебл число(int?). Метод вертає 0 якщо прийов null, в іншому випадку саме число. Перевірку зробити двома способами(if HasValue i ??)
+namespace Chapter4.Task10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
