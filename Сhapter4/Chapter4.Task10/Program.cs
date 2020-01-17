fa65aef18fbb85e2846d05c29579c92b82175821
lolissimo2402 <lolissimo2402@gmail.com>
2020-01-17 16:09:29 +0200

    Commit with first method

--- Сhapter4/Chapter4.Task10/Program.cs
+++ Сhapter4/Chapter4.Task10/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 //10. Написати метод, який приймає приймає налебл число(int?). Метод вертає 0 якщо прийов null, в іншому випадку саме число. Перевірку зробити двома способами(if HasValue i ??)
 namespace Chapter4.Task10
 {
@@ -9,6 +8,24 @@ namespace Chapter4.Task10
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your number");
+
+                Nullable<int> a = new Nullable<int>(1);
+                Nullable<int> b = null;
+                Nullable<int> c = new Nullable<int>(2);
+
+                Nullable<int>[] array = { a, b, c };
+            foreach (int? num in array)
+            {
+                if (num.HasValue)
+                {
+                    Console.WriteLine($"If method return number = {num.Value}");
+                }
+                else
+                {
+                    Console.WriteLine($"If method return null = {0}");
+                }
+            }
 
         }
     }
