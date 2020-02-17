258906ff048f900db3b086b919cd0afc175f1369
lolissimo2402 <lolissimo2402@gmail.com>
2020-02-17 19:07:28 +0200

    Chapter5.Task1

---
+++ Chapter5/Chapter5.Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Chapter5.Task1
+{
+    class Program
+    {
+        int sum;
+        int dif;
+        int mult;
+        const int a = 10;
+        private static readonly int num;
+
+        static Program()
+        {
+            Console.WriteLine("Enter your number");
+            string str = Console.ReadLine();
+            int b = int.Parse(str);
+            num = b;
+        }
+
+        private void Operation()
+        {
+            sum = a + num;
+            dif = a - num;
+            mult = a * num;
+            Console.WriteLine($"Sum = {sum}, dif = {dif}, mult = {mult} ");
+        }
+
+        static void Main(string[] args)
+        {
+            Program c = new Program();
+            c.Operation();
+        }
+    }
+}
