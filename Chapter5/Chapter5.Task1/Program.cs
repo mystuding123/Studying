305d00cae9d1381335ba6ff992bade5b41dbe75c
lolissimo2402 <lolissimo2402@gmail.com>
2020-02-18 10:44:01 +0200

    Chapter5.Task1 update

--- Chapter5/Chapter5.Task1/Program.cs
+++ Chapter5/Chapter5.Task1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+//1. 	- В класі Program оголосити:
+//константу типу int
+//приватне статичне readonly поле типу int, якому в статичному констркукторі присвоюється якесь значення(зчитується з консолі)
+//    в методі мейн виводиться на екран добуток сума і різниця константи і статичного поля
 namespace Chapter5.Task1
 {
     class Program
@@ -10,10 +13,9 @@ namespace Chapter5.Task1
         {
             Console.WriteLine("Enter your number");
             string str = Console.ReadLine();
-            int b = int.Parse(str);
-            num = b;
+            num = int.Parse(str);
         }
-        private void Operation()
+        static void Operation()
         {
            int sum = a + num;
            int dif = a - num;
@@ -22,8 +24,7 @@ namespace Chapter5.Task1
         }
         static void Main(string[] args)
         {
-            Program c = new Program();
-            c.Operation();
+            Operation(); 
         }
     }
 }
